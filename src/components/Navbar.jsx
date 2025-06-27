d04f30784cec246d338856d964a2429f03b3a696
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-28 00:07:52 +0530

    Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+
+const Navbar = () => {
+  return (
+    <div className='h-[60px] gap-2 text-xl font-medium bg-white my-4 rounded-lg flex items-center justify-center'>
+             <img src="/firebase.svg" alt="logo" />
+             <h1>Firebase Contact App</h1>
+    </div>
+  )
+}
+
+export default Navbar
